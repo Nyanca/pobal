119fdb63a1ac0fd03123d00f6c565c2983a217f6
aoife kavanagh <nyhankaa@tcd.ie>
2018-11-12 19:55:42 +0000

    added new path for postgres DB and imported dj_database_url

--- pobal/settings.py
+++ pobal/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -76,13 +77,13 @@ WSGI_APPLICATION = 'pobal.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
+# DATABASES = {
+#     'default': {
+#         # 'ENGINE': 'django.db.backends.sqlite3',
+#         # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+DATABASES = {'default':dj_database_url.parse('postgres://qgaqeztvarguuk:e864334ff24fa225f284ed63c84c85d9eb5766b7c11e00ec5d868effb9bb0d1b@ec2-54-75-242-47.eu-west-1.compute.amazonaws.com:5432/ddncq466ped7no')}
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
