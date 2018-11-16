4ebde4523e6cb459ddb0032cc43ede61f3e91042
aoife kavanagh <nyhankaa@tcd.ie>
2018-11-16 13:44:30 +0000

    added dynamic media url & root security url

--- pobal/urls.py
+++ pobal/urls.py
@@ -15,12 +15,17 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.views.generic import RedirectView
+from django.views.static import serve
 from accounts import urls as urls_accounts
 from pobalStudio import urls as urls_studio
+from .settings import MEDIA_ROOT
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', RedirectView.as_view(url='pobal/')),
     url(r'^accounts/', include(urls_accounts)),
     url(r'^pobal/', include(urls_studio)),
+    url(r'^media/(?P<path>.*)$', serve, {'document_root': MEDIA_ROOT}),
     
 ]
