5d33913bdff1928c46c4d8d8f73af2a64c78a375
aoife kavanagh <nyhankaa@tcd.ie>
2018-11-16 22:45:34 +0000

    added create or edit ticket functionality

--- pobalStudio/urls.py
+++ pobalStudio/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import url
-from .views import pobal_studio, new_ticket, ticket_form, get_all_tickets
+from .views import pobal_studio, ticket_form, get_all_tickets, ticket_detail,create_or_edit_ticket
 
 urlpatterns = [
     url(r'^pobal_studio/$', pobal_studio, name='pobal_studio'),
-    url(r'^new_ticket/$', new_ticket, name='new_ticket'),
+    url(r'^new/$', create_or_edit_ticket, name='new_ticket'),
     url(r'^ticket_form/$', ticket_form, name='ticket_form'),
     url(r'^get_all_tickets/$', get_all_tickets, name='tickets'),
+    url(r'^(?P<pk>\d+)/$', ticket_detail, name='ticket_detail'),
+    url(r'^(?P<pk>\d+)/edit/$', create_or_edit_ticket, name='edit_ticket'),
 ]
--- pobalStudio/views.py
+++ pobalStudio/views.py
@@ -17,28 +17,51 @@ def ticket_form(request):
     
     return render(request, 'ticket_form.html', args)
     
-def new_ticket(request):
-    # a view to manage the create ticket form
-    if request.method=='POST':
-        ticketForm = CreateTicketForm(request.POST, request.FILES)
+# def new_ticket(request):
+#     # a view to manage the create ticket form
+#     if request.method=='POST':
+#         ticketForm = CreateTicketForm(request.POST, request.FILES)
 
-        if ticketForm.is_valid():
-            data = ticketForm.cleaned_data
+#         if ticketForm.is_valid():
+#             data = ticketForm.cleaned_data
             
-            title = data['title']
-            summary = data['summary']
-            detail = data['detail']
-            image = data['image']
+#             title = data['title']
+#             summary = data['summary']
+#             detail = data['detail']
+#             image = data['image']
 
-            form_data = Ticket(title=title, summary=summary, detail=detail, image=image)
-            form_data.save()
+#             form_data = Ticket(title=title, summary=summary, detail=detail, image=image)
+#             form_data.save()
         
-        args = {'title':title, 'summary':summary, 'detail':detail, 'image':image}
-        return render(request, 'all_tickets.html', args)
+#         args = {'title':title, 'summary':summary, 'detail':detail, 'image':image}
+#         return redirect(request, get_all_tickets, args)
 
 def get_all_tickets(request):
     # a view to get all tickets saved to the DB
     all_tickets = Ticket.objects.filter(date__lte=timezone.now
     ()).order_by('-date')
-    
     return render(request, 'all_tickets.html', {'all_tickets':all_tickets})
+    
+def ticket_detail(request, pk):
+    # a view that renders a single ticket in full detail or returns 404 if not found
+    
+    ticket = get_object_or_404(Ticket, pk=pk)
+    ticket.views += 1
+    ticket.save()
+    
+    return render(request, 'ticket_detail.html', {'ticket':ticket})
+    
+def create_or_edit_ticket(request, pk=None):
+    # get the selected ticket by id if it exists
+    ticket = get_object_or_404(Ticket, pk=pk) if pk else None
+    
+    # get the form data for the selected ticket
+    if request.method == 'POST':
+        ticket_form = CreateTicketForm(request.POST, request.FILES, instance=ticket)
+        if ticket_form.is_valid():
+            ticket = ticket_form.save()
+            return redirect(ticket_detail, ticket.pk)
+    else:
+        ticket_form = CreateTicketForm(instance=ticket)
+        
+    return render(request, 'ticket_form.html', {'ticketForm':ticket_form})
