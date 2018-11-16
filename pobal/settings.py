4a1b3472a602576b8b23f5bc285532482a2d39c3
aoife kavanagh <nyhankaa@tcd.ie>
2018-11-16 13:10:10 +0000

    added STATIC_ROOT path

--- pobal/settings.py
+++ pobal/settings.py
@@ -89,24 +89,6 @@ WSGI_APPLICATION = 'pobal.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         # 'ENGINE': 'django.db.backends.sqlite3',
-#         # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
-# if development:
-#     DATABASES = {
-#         'default': {
-#             'ENGINE': 'django.db.backends.sqlite3',
-#             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#         }
-#     }
-#     # print("Postgres URL not found, using sqlite instead")
-# else:
-#     DATABASES = {'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))}  
-
 if development:
     if "DATABASE_URL" in os.environ:    
         DATABASES = {'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))} 
@@ -161,6 +143,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
