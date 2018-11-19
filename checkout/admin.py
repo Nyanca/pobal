9b3727c0a6fb2af0b0a23f5c488e4f6a8352a347
aoife kavanagh <nyhankaa@tcd.ie>
2018-11-19 19:09:53 +0000

    added models & admin models for order details(user & product summary)

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Order, OrderLine
 
-# Register your models here.
+class OrderLineAdminInline(admin.TabularInline):
+    # render subform as tabular inline style
+    model = OrderLine
+
+class OrderAdmin(admin.ModelAdmin):
+    # create inlines for single page editing of mulitiple models
+    inlines = (OrderLineAdminInline, )
+
+admin.site.register(Order, OrderAdmin)
