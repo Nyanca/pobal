8c452907923e7ea6fd0535ff644831bb7b64aae1
aoife kavanagh <nyhankaa@tcd.ie>
2018-11-19 17:49:09 +0000

    working on cart views & urls for purchasing tickets. Added basic template

---
+++ cart/contexts.py
@@ -0,0 +1,22 @@
+from django.shortcuts import get_object_or_404
+from products.models import Ticket
+
+def cart_contents(request):
+    # a cart view to render a shopping cart throughout the Pobal app
+    
+    # get an exisiting cart or initialize new cart
+    cart = request.session.get('cart', {})
+    
+    # initialize cart variables
+    cart_items = []
+    product_count = 0 
+    total = 0
+    
+    # give each cart variable a value and add these values to the cart
+    for id, quantity in cart.items():
+        ticket = get_object_or_404(Ticket, pk=id)
+        total += quantity * ticket.price
+        product_count += quantity
+        cart_items.append({'id':id, 'quantity':quantity, 'ticket':ticket})
+        
+    return({'cart_items': cart_items, 'total':total, 'product_count':product_count})
--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse, get_object_or_404, render_to_response
+from django.utils import timezone
 
-# Create your views here.
+def view_cart(request):
+    # renders a users' shopping cart with contents if any
+    return(request, 'cart.html')
+    
+def add_to_cart(request, id):
+    # appends the quantity of an item to the cart within the current session using item id 
+    quantity = int(request.POST.get('quantity'))
+    
+    cart = request.session.get('cart', {})
+    cart[id] = cart.get(id, quantity)
+    
+    request.session['cart'] = cart
+    return redirect(reverse('index'))
+    
+def edit_cart(request):
+    # modifies the cart within the current session using item id 
+    quantity = int(request.POST.get('quantity'))
+    cart = request.session.get('cart', {})
+    
+    if quantity > 0:
+        cart[id] = quantity
+    else:
+        cart.pop(id)
+        
+    request.session['cart'] = cart
+    return redirect(reverse('view_cart'))
