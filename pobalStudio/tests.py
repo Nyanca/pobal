4a49ee167951d460cc21954a4b6b4b3d8bf29329
aoife kavanagh <nyhankaa@tcd.ie>
2018-11-27 11:53:49 +0000

    removed old test file

--- pobalStudio/tests.py
+++
@@ -1,7 +0,0 @@
-from django.test import TestCase
-from .models import Ticket, Comment
-
-class TicketTests(TestCase):
-    def test_str(self):
-        test_name = Ticket(title = 'A Ticket')
-        self.assertEqual(str(test_name), 'A Ticket')
