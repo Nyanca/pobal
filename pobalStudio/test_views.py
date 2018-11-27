e7caea3cae38fd0f1367c802820f60891bfd774e
aoife kavanagh <nyhankaa@tcd.ie>
2018-11-27 13:35:15 +0000

    added more test to views

--- pobalStudio/test_views.py
+++ pobalStudio/test_views.py
@@ -1,7 +1,9 @@
 from django.test import TestCase
 from django.shortcuts import render, reverse, get_object_or_404
+from django.contrib.auth import get_user_model
 from .models import Ticket
 from .views import pobal_studio, ticket_form, get_all_tickets, ticket_detail, ticket_like_toggle, add_comment, create_or_edit_ticket, delete_ticket
+from .forms import CommentForm
 
 class TestViewRoutes(TestCase):
     def test_get_ticket_form_route(self):
@@ -38,6 +40,12 @@ class TestViewRoutes(TestCase):
         self.assertEqual(page.status_code, 200)
         self.assertTemplateUsed(page, 'ticket_form.html')
         
+    def test_create_or_edit_ticket_form_for_idNONE(self):
+        # test that 404 error is raised if ticket not found
+        page = self.client.get('/pobal_studio/1/edit/')
+        self.assertEqual(page.status_code, 404)
+        print('test_create_or_edit_ticket_form_for_NONE_route: Cannot edit ticket. Ticket does not exist')
+
     def test_delete_ticket_route(self):
         ticket = Ticket(title='a ticket', summary='a ticket summary', detail='ticket detail')
         ticket.save()
@@ -45,8 +53,28 @@ class TestViewRoutes(TestCase):
         page = self.client.get('/pobal_studio/{0}/delete/'.format(ticket.id))
         self.assertEqual(page.status_code, 200)
         self.assertTemplateUsed(page, 'all_tickets.html')
-    
+        
+class OtherOtherFeatures(TestCase):
+    def test_timezone_now(self):
+        # compare the date stamp on two tickets saved one after the other 
+        ticket = Ticket(title='a ticket', summary='a ticket summary', detail='ticket detail')
+        ticket.save()
+        
+        ticket2 = Ticket(title='a 2nd ticket', summary='a 2nd ticket summary', detail='more ticket detail')
+        ticket2.save()
+        self.assertTrue(ticket.date < ticket2.date)
+        
+    def test_view_incrementation_on_page_load(self):
+        # create ticket for accessing urls that contain ticket id's
+        ticket = Ticket(title='a ticket', summary='a ticket summary', detail='ticket detail')
+        ticket.save()
+        
+        # get ticket_detail page by ticket id created above
+        page = self.client.get('/pobal_studio/{0}/'.format(ticket.id))
+        
+        # hardcode page load to test view incrementation functionality    
+        if self.assertTemplateUsed(page, 'ticket_detail.html') is True:
+            ticket.views += 1
+            self.assertEqual(ticket.views, 1)
     
         
-        
-    
