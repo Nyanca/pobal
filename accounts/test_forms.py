615e11a97d5a0faaa4e2885e49b1e1a31fe84405
aoife kavanagh <nyhankaa@tcd.ie>
2018-11-28 14:14:41 +0000

    added tests for login form and reg form

--- accounts/test_forms.py
+++ accounts/test_forms.py
@@ -1,4 +1,5 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.contrib.auth.models import User
 from django.contrib.auth.password_validation import (
     CommonPasswordValidator, MinimumLengthValidator, NumericPasswordValidator,
     UserAttributeSimilarityValidator, get_default_password_validators,
@@ -9,11 +10,15 @@ from .forms import UserLoginForm, UserRegistrationForm
 
 class TestUserLoginForm(TestCase):
     def test_user_form_requires_two_fields(self):
-        form = UserLoginForm({'username_or_email':'test_user', 'password':'12345'})
-        self.assertTrue(form.is_valid())
+        form = UserLoginForm({'username_or_email':'test_user'})
+        self.assertFalse(form.is_valid())
         
     def test_user_form_can_take_email_and_password_combo(self):
         form = UserLoginForm({'username_or_email':'test_user@example.com', 'password':'12345'})
+        self.assertTrue(form.is_valid())
+    
+    def test_user_login_takes_username_and_password_combo(self):
+        form = UserLoginForm({'username_or_email':'test_user', 'password':'12345'})
         self.assertTrue(form.is_valid())
 
 class TestPasswordValidation(TestCase):
@@ -36,3 +41,73 @@ class TestPasswordValidation(TestCase):
         with self.assertRaises(ValidationError) as error:
             validate_password('password')
         self.assertEqual(error.exception.messages, ['This password is too common.'])
+
+    def test_password_validation_for_numeric_string_is_false(self):
+        with self.assertRaises(ValidationError) as error:
+            validate_password('16722349')
+        self.assertEqual(error.exception.messages, ["This password is entirely numeric."])
+        
+    def test_password_can_not_be_too_common_or_entirely_numeric(self):
+        with self.assertRaises(ValidationError) as error:
+            validate_password('12345678')
+        self.assertEqual(error.exception.messages, ['This password is too common.', 'This password is entirely numeric.'])
+        
+class TestRegistrationForm(TestCase):
+    def test_is_valid_with_four_fields(self):
+        form = UserRegistrationForm({
+            'username':'test_user',
+            'email':'test_user@example.com',
+            'password1':'12345',
+            'password2':'12345'
+        })
+        self.assertTrue(form.is_valid())
+        
+    def test_NOT_valid_without_two_password_fields(self):
+        form = UserRegistrationForm({
+            'username':'test_user',
+            'email':'test_user@example.com',
+            'password2':'12345'
+        })
+        self.assertFalse(form.is_valid())
+    
+    def test_NOT_valid_with_different_passwords(self):
+        form = UserRegistrationForm({
+            'username':'test_user',
+            'email':'test_user@example.com',
+            'password1':'12345',
+            'password2':'67987'
+        })
+        self.assertFalse(form.is_valid())
+        self.assertEqual(form.errors, {'password2': ['Passwords do not match']})
+        
+    def test_NOT_valid_with_existing_user(self):
+         #create a new user
+        User.objects.create_user(username='existing_user',
+                                 email='user@example.com',
+                                 password='user')
+                                 
+        # pass the new user' username the reg form
+        form = UserRegistrationForm({
+            'username':'existing_user',
+            'email':'test_user@example.com',
+            'password1':'12345',
+            'password2':'12345'
+        })
+        self.assertFalse(form.is_valid())
+        self.assertEqual(form.errors, {'username': ['A user with that username already exists.']})
+        
+    def test_email_must_be_unique(self):
+         #create a new user
+        User.objects.create_user(username='existing_user',
+                                 email='user@example.com',
+                                 password='user')
+                                 
+        # pass the new user' username the reg form
+        form = UserRegistrationForm({
+            'username':'test_user',
+            'email':'user@example.com',
+            'password1':'12345',
+            'password2':'12345'
+        })
+        self.assertFalse(form.is_valid())
+        self.assertEqual(form.errors, {'email': ['Email addresses must be unique.']})
