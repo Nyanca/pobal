2361dd3f438de5d46342205ea53f2f9688135df5
aoife kavanagh <nyhankaa@tcd.ie>
2018-11-28 17:06:16 +0000

    finished search tests

--- search/tests.py
+++ search/tests.py
@@ -8,3 +8,8 @@ class TestSearchView(TestCase):
         page = self.client.get('/search/')
         self.assertEqual(page.content, (b'Please submit a search term.'))
         self.assertTemplateNotUsed(page, 'all_tickets.html')
+        
+    def test_search_route_renders_expected_template(self):
+        # show that request returns template when vaue 'q' is found
+        response = self.client.get('/search/', {'q': 'a value'})
+        self.assertTemplateUsed(response, 'all_tickets.html')
