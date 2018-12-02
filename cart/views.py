ae79ae342de032dc0c011942d1d42205e0df5cf9
aoife kavanagh <nyhankaa@tcd.ie>
2018-12-02 18:08:40 +0000

    added delete item from cart

--- cart/views.py
+++ cart/views.py
@@ -13,17 +13,17 @@ def add_to_cart(request, id):
     cart[id] = cart.get(id, quantity)
     
     request.session['cart'] = cart
-    return redirect(reverse, 'home')
+    return redirect(reverse('home'))
     
-def edit_cart(request):
+def edit_cart(request, id):
     # modifies the cart within the current session using item id 
-    quantity = int(request.POST.get('quantity'))
+    ticket = request.POST.get('ticket_id')
     cart = request.session.get('cart', {})
     
-    if quantity > 0:
-        cart[id] = quantity
-    else:
+    if ticket:
         cart.pop(id)
         
     request.session['cart'] = cart
+    request.session.modified = True
+    
     return redirect(reverse('view_cart'))
