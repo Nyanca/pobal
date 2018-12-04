b56f35c614cb01fe1cfaae98c20d6c4b79889ab5
aoife kavanagh <nyhankaa@tcd.ie>
2018-12-04 14:10:40 +0000

    added new postgres db url

--- pobal/settings.py
+++ pobal/settings.py
@@ -95,7 +95,7 @@ WSGI_APPLICATION = 'pobal.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 if "DATABASE_URL" in os.environ:    
-    DATABASES = {'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))} 
+    DATABASES = {'default': dj_database_url.parse(os.environ.get('HEROKU_POSTGRESQL_GREEN_URL'))} 
      
 else:      
     print("Postgres URL not found, using sqlite instead")     
